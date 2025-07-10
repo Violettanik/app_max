e3146431e4cee53b72dcd102d319aa27ef13de45
Violettanik <146526302+Violettanik@users.noreply.github.com>
2025-07-10 19:41:48 +0300

    Update auth.py

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -3,14 +3,13 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 from sqlalchemy.orm import Session
-
+from ..main import AUTH_ERRORS
 from ..models import User, Image
 from ..database import get_db
 
 router = APIRouter()
 templates = Jinja2Templates(directory=str(Path(__file__).parent.parent / "templates"))
 
-# Регистрация
 @router.get("/register", response_class=HTMLResponse)
 async def register_page(request: Request):
     return templates.TemplateResponse("register.html", {"request": request})
@@ -23,27 +22,26 @@ async def register_user(
     password: str = Form(...),
     db: Session = Depends(get_db)
 ):
-    # Проверка существующего пользователя
     if db.query(User).filter(User.username == username).first():
+        AUTH_ERRORS.inc()
         return templates.TemplateResponse("register.html", {
             "request": request,
             "error": "Username already exists"
         })
     
     if db.query(User).filter(User.email == email).first():
+        AUTH_ERRORS.inc()
         return templates.TemplateResponse("register.html", {
             "request": request,
             "error": "Email already exists"
         })
     
-    # Создание пользователя (пароль хранится открыто - только для демо!)
     user = User(username=username, email=email, password=password)
     db.add(user)
     db.commit()
     
     return RedirectResponse(url="/login", status_code=303)
 
-# Вход
 @router.get("/login", response_class=HTMLResponse)
 async def login_page(request: Request):
     return templates.TemplateResponse("login.html", {"request": request})
@@ -57,18 +55,17 @@ async def login(
 ):
     user = db.query(User).filter(User.username == username).first()
     
-    if not user or user.password != password:  # Сравнение открытых паролей!
+    if not user or user.password != password:
+        AUTH_ERRORS.inc()
         return templates.TemplateResponse("login.html", {
             "request": request,
             "error": "Invalid credentials"
         })
-
-#    response.set_cookie(key="username", value=username)    
+ 
     response = RedirectResponse(url="/profile", status_code=303)
     response.set_cookie(key="username", value=username)
     return response
 
-# Профиль
 @router.get("/profile", response_class=HTMLResponse)
 async def profile_page(request: Request, username: str = "", db: Session = Depends(get_db)):
     username = request.cookies.get("username")
@@ -89,5 +86,5 @@ async def profile_page(request: Request, username: str = "", db: Session = Depen
 @router.post("/logout")
 async def logout():
     response = RedirectResponse(url="/login", status_code=303)
-    response.delete_cookie("username")  # Удаляем куки с именем пользователя
+    response.delete_cookie("username")
     return response
